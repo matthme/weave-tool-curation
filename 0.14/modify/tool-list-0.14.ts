a4605404a371d948785b572c3ecbccd1c4e40e96
Matthias <tuttiacc@hotmail.com>
2025-07-11 19:52:01 +0200

    add Rhymez

--- 0.14/modify/tool-list-0.14.ts
+++ 0.14/modify/tool-list-0.14.ts
@@ -36,5 +36,31 @@ export default defineDevCollectiveToolList({
         },
       ],
     },
+    {
+      id: "matthme.rhymez",
+      versionBranch: "0.1.x",
+      title: "Rhymez",
+      subtitle: "Threaded Chat and DMs",
+      description:
+        "Chat app that supports channels, direct messages to friends and threads at the side in panels. It's time for fun and test, rhyming is allowed. And above all what is best - it's peer-to-peer (not in the cloud). Still it is experimental though - not expecting should you therefore dare, being able to re-import your data (this is alpha, note, not beta) once a newer version is out there.",
+      icon: "https://github.com/matthme/presence/blob/main/ui/icon.png?raw=true",
+      tags: ["chate", "messages", "message", "threads", "channels"],
+      versions: [
+        {
+          version: "0.1.0",
+          url: "https://codeberg.org/matthme/rhymez/releases/download/0.1.0/rhymez.webhapp",
+          hashes: {
+            happSha256:
+              "bbf796ebe322eca972cb32175881c2e5f6d8972e465863653f6e2d87502d0ff8",
+            webhappSha256:
+              "499885742be5b85f339c3f042bc618e067978e48ea2181f0cbf73520cf184ca0",
+            uiSha256:
+              "7ef4d498e3ad88309d789043a987410f35e458dd5644e527c93289faeae109c3",
+          },
+          changelog: "The first experimental release of Rhymez",
+          releasedAt: 1752256287974,
+        },
+      ],
+    },
   ],
 });
