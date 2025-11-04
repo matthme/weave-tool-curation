b2a1c3696e0539a18aae4dfb79dcf374102b03d6
Matthias <tuttiacc@hotmail.com>
2025-11-04 17:03:31 +0100

    add lists for version 0.15

---
+++ 0.15/modify/curations-0.15.ts
@@ -0,0 +1,20 @@
+import { defineCurationLists } from "@theweave/moss-types";
+
+export default defineCurationLists({
+  curator: {
+    name: "matthme",
+    description: "matthme's opinionated curations",
+    contact: {
+      website: "https://github.com/matthme",
+    },
+    icon: "https://avatars.githubusercontent.com/u/36768177?v=4",
+  },
+  curationLists: {
+    default: {
+      name: "Default Curation List",
+      description: "matthme's opinionated curations",
+      tags: [],
+      tools: [],
+    },
+  },
+});
